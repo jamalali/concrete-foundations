92136ce4e1f23a6a8f4593c9381e447555054a59
Jamal <Jamal@Jamal-PC.default>
2014-08-19 23:38:41 +0100

    Modernizr added

--- elements/header.php
+++ elements/header.php
@@ -1,51 +1,57 @@
 <?php  defined('C5_EXECUTE') or die("Access Denied."); ?>
 <!DOCTYPE html>
-<html lang="<?php echo LANGUAGE?>">
+<html class="no-js" lang="en" lang="<?php echo LANGUAGE?>">
 
-<head>
+    <head>
 
-<?php  Loader::element('header_required'); ?>
+        <?php  Loader::element('header_required'); ?>
 
-<!-- Site Header Content //-->
+        <!-- Site Header Content //-->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        
+        <script src="<?php echo $this->getThemePath(); ?>/bower_components/modernizr/modernizr.js"></script>
 
-<link rel="stylesheet" href="<?php echo $this->getThemePath(); ?>/css/reset.css" />
-<link rel="stylesheet" href="<?php echo $this->getThemePath(); ?>/css/text.css" />
-<link rel="stylesheet" href="<?php echo $this->getThemePath(); ?>/css/960_24_col.css" />
-<link rel="stylesheet" media="screen" type="text/css" href="<?php echo $this->getStyleSheet('main.css')?>" />
-<link rel="stylesheet" media="screen" type="text/css" href="<?php echo $this->getStyleSheet('typography.css')?>" />
-<link href='//fonts.googleapis.com/css?family=Merriweather:400,700,900,300' rel='stylesheet' type='text/css' />
+        <link rel="stylesheet" href="<?php echo $this->getThemePath(); ?>/css/reset.css" />
+        <link rel="stylesheet" href="<?php echo $this->getThemePath(); ?>/css/text.css" />
+        <link rel="stylesheet" href="<?php echo $this->getThemePath(); ?>/css/960_24_col.css" />
+        <link rel="stylesheet" media="screen" type="text/css" href="<?php echo $this->getStyleSheet('css/main.css')?>" />
+        <link rel="stylesheet" media="screen" type="text/css" href="<?php echo $this->getStyleSheet('css/typography.css')?>" />
+        <link href='//fonts.googleapis.com/css?family=Merriweather:400,700,900,300' rel='stylesheet' type='text/css' />
+
+    </head>
+
+    <body>
+        
+        <div class="cf-ui">
+
+            <div class="row">
+                
+                <div class="small-12 columns">
+
+                    <div id="main-container" class="main-container container_24">
+
+                        <div id="header">
 
 
-</head>
+                            <?php 
+                            $a = new GlobalArea('Site Name');
+                            $a->display();
+                            ?>
 
-<body>
+                            <?php 
+                            $a = new GlobalArea('Header Nav');
+                            $a->display();
+                            ?>
 
-<!--start main container -->
+                            <div id="header-image">
 
-<div id="main-container" class="container_24">
+                                    <?php 
+                                    $a = new Area('Header Image');
+                                    $a->display($c);
+                                    ?>
 
-	<div id="header">
-	
-	
-		<?php 
-		$a = new GlobalArea('Site Name');
-		$a->display();
-		?>
+                            </div>
 
-		<?php 
-		$a = new GlobalArea('Header Nav');
-		$a->display();
-		?>
-		
-		<div id="header-image">
-		
-			<?php 
-			$a = new Area('Header Image');
-			$a->display($c);
-			?>
-		
-		</div>
-		
-	</div>
-	
-	<div class="clear"></div>
+                        </div>
+
+                        <div class="clear"></div>
